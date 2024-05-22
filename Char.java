90d020061084f86fa34de13330b02bef6cf5d0bc
Rafael23082 <144778682+Rafael23082@users.noreply.github.com>
2024-05-22 16:57:47 +0700

    Update Char.java

--- Char.java
+++ Char.java
@@ -2,69 +2,14 @@ import java.util.*;
 public class Char{
     public static void main(String[] args){
 
-        String[] characterContainer = {"Acheron","Argenti","Arlan","Asta","Aventurine","Bailu","Blackswan","Blade","Bronya",
-                "Clara","Danheng","Dr.ratio","Fuxuan","Gallagher","Gepard","Guinaifen","Hanya","Herta","Himeko",
-                "Hook","Huohuo","Imbibitorlunae","Jingyuan","Jingliu","Kafka","Luka","Luocha","Lynx","March7th",
-                "Misha","Natasha","Pela","Qingque","Robin","Ruanmei","Sampo","Seele","Serval","Silverwolf","Sparkle",
-                "Sushang","Tingyun","Topaz","Trailblazer","Welt","Xueyi","Yanqing","Yukong"};
-
-        String[][] characterInformation = {
-                {"5 Star","Nihility","Lightning","Self Anihilator"},
-                {"5 Star","Erudition","Physical","Knights of Beauty"},
-                {"4 Star","Destruction","Lightning","Herta Space Station"},
-                {"4 Star","Harmony","Fire","Herta Space Station"},
-                {"5 Star","Preservation","Imaginary","IPC"},
-                {"5 Star","Abundance","Lightning","The Xianzhou Loufu"},
-                {"5 Star","Nihility","Wind","Garden of Recollection"},
-                {"5 Star","Destruction","Wind","Stellaron Hunter"},
-                {"5 Star","Harmony","Wind","Belobog"},
-                {"5 Star","Destruction","Physical","Belobog"},
-                {"4 Star","The Hunt","Wind","Astral Express"},
-                {"5 Star","The Hunt","Imaginary","Intelligentsia Guild"},
-                {"5 Star","Preservation","Quantum","The Xianzhou Loufu"},
-                {"4 Star","Abundance","Fire","Penacony"},
-                {"5 Star","Preservation","Ice","Belobog"},
-                {"4 Star","Nihility","Fire","The Xianzhou Loufu"},
-                {"4 Star","Harmony","Physical","Xianzhou Loufu"},
-                {"4 Star","Erudition","Ice","Herta Space Station"},
-                {"5 Star","Erudition","Fire","Astral Express"},
-                {"4 Star","Destuction","Fire","Belobog"},
-                {"5 Star","Abundance","Wind","The Xianzhou Loufu"},
-                {"5 Star","Destruction","Imaginary","Astral Express"},
-                {"5 Star","Erudition","Lightning","The Xianzhou Loufu"},
-                {"5 Star","Destruction","Ice","The Xianzhou Loufu"},
-                {"5 Star","Nihility","Lightning","Stellaron hunter"},
-                {"4 Star","Nihility","Physical","Belobog"},
-                {"5 Star","Abundance","Imaginary","The Xianzhou Loufu"},
-                {"4 Star","Abundance","Quantum","Belobog"},
-                {"4 Star","Preservation","Ice","Astral Express"},
-                {"4 Star","Destruction","Ice","Penacony"},
-                {"4 Star","Abundance","Physical","Belobog"},
-                {"4 Star","Nihilty","Ice","Belobog"},
-                {"4 Star","Erudition","Quantum","The Xianzhou Loufu"},
-                {"5 Star","Harmony","Physical","Penacony"},
-                {"5 Star","Harmony","Ice","Herta Space Station"},
-                {"4 Star","Nihility","Wind","Belobog"},
-                {"5 Star","The Hunt","Quantum","Belobog"},
-                {"4 Star","Erudition","Lightning","Belobog"},
-                {"5 Star","Nihility","Quantum","Stellaron Hunter"},
-                {"5 Star","Harmony","Quantum","Masked Fools"},
-                {"4 Star","The Hunt","Physical","The Xianzhou Loufu"},
-                {"4 Star","Harmony","Lightning","The Xianzhou Loufu"},
-                {"5 Star","The Hunt","Fire","IPC"},
-                {"5 Star","Adaptive","Adaptive","Astral Express"},
-                {"5 Star","Nihility","Imaginary","Astral Express"},
-                {"4 Star","Destruction","Quantum","The Xianzhou Loufu"},
-                {"5 Star","The Hunt","Ice","The Xianzhou Loufu"},
-                {"4 Star","Harmony","Imaginary","The Xianzhou Loufu"}
-        };
+        Character chars = new Character();
 
         String[] Action_List = {"A", "B", "C", "D", "Q"};
 
         Hashtable<String, String[]> Retrieve_Information = new Hashtable<>(48);
 
-        for (int i = 0; i < characterContainer.length; i++){
-            Retrieve_Information.put(characterContainer[i], characterInformation[i]);
+        for (int i = 0; i < chars.characterContainer.length; i++){
+            Retrieve_Information.put(chars.characterContainer[i], chars.characterInformation[i]);
         }
 
         Scanner scn = new Scanner(System.in);
@@ -95,19 +40,19 @@ public class Char{
 
             switch (action.toUpperCase()) {
                 case "A":
-                    String character = getCharacter(scn, characterContainer);
+                    String character = getCharacter(scn, chars.characterContainer);
                     getInfo(character, Retrieve_Information, 0);
                     loop = proceed();
                     break;
 
                 case "B":
-                    character = getCharacter(scn, characterContainer);
+                    character = getCharacter(scn, chars.characterContainer);
                     getInfo(character, Retrieve_Information, 1);
                     loop = proceed();
                     break;
 
                 case "C":
-                    character = getCharacter(scn, characterContainer);
+                    character = getCharacter(scn, chars.characterContainer);
                     getInfo(character, Retrieve_Information, 2);
                     loop = proceed();
                     break;
@@ -116,7 +61,7 @@ public class Char{
                     System.out.println("                                         || Printing Characters Information ||\n ");
                     System.out.println("Character:          Rarity:             Path:               Element:            Faction:");
 
-                    printWithSpace(characterContainer, characterInformation);
+                    printWithSpace(chars.characterContainer, chars.characterInformation);
                     loop = proceed();
                     break;
 
