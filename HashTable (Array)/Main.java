a3d82f77fe6c136ef4e1b07796eef71eef848b30
Rafael23082 <144778682+Rafael23082@users.noreply.github.com>
2024-06-01 12:25:18 +0700

    Update Main.java

--- HashTable (Array)/Main.java
+++ HashTable (Array)/Main.java
@@ -1,12 +1,14 @@
 import java.util.*;
 
+// To know what the methods do, please refer to the Methods.java file.
+
 public class Main extends Methods {
     public static void main(String[] args) {
 
         Character chars = new Character();
 
+        // Initializing the hashtable and adding elements (Name : Array of Information)
         Hashtable<String, String[]> Retrieve_Information = new Hashtable<>(48);
-
         for (int i = 0; i < chars.characterContainer.length; i++) {
             Retrieve_Information.put(chars.characterContainer[i], chars.characterInformation[i]);
         }
@@ -14,41 +16,47 @@ public class Main extends Methods {
         Scanner scn = new Scanner(System.in);
 
         System.out.println("Welcome to a Honkai Star Rail Database System");
-        boolean loop = true;
+        boolean loop = true; // If the loop becomes false, the program will stop (Determined by he method proceed)
 
         while (loop) {
             System.out.println("----------------------------------\nA: Get Rarity\nB: Get Path\nC: Get Element\nD: Print all Characters\nE: Filter Characters\nF: Display Information of a Chracter\nG: Sort Characters\nQ: Quit\n----------------------------------\nPlease enter an action you want to perform: ");
 
-            boolean action_present = false;
             String action = "";
 
-            action = checkInput(scn, new String[]{"A", "B", "C", "D", "E", "F", "G", "Q"});
+            action = checkInput(scn, new String[]{"A", "B", "C", "D", "E", "F", "G", "Q"}); // To ensure user input is valid
 
+            // To perform different actions based on user input
             switch (action.toUpperCase()) {
+
+                // Getting Rarity
                 case "A":
                     String character = getCharacter(scn, chars.characterContainer);
                     getInfo(character, Retrieve_Information, 0);
                     loop = proceed();
                     break;
 
+                // Getting Path
                 case "B":
                     character = getCharacter(scn, chars.characterContainer);
                     getInfo(character, Retrieve_Information, 1);
                     loop = proceed();
                     break;
 
+                // Getting Element
                 case "C":
                     character = getCharacter(scn, chars.characterContainer);
                     getInfo(character, Retrieve_Information, 2);
                     loop = proceed();
                     break;
 
+                // Printing all Characters
                 case "D":
                     System.out.println("                                         || Printing Characters Information ||\n ");
                     printAllCharacters(chars.characterContainer, chars.characterInformation);
                     loop = proceed();
                     break;
 
+                // Filtering
                 case "E":
                     System.out.print("Filter Characters By: \nA: Alphabet\nB: Element\nC: Path\nD: Faction\nE: Rarity\n");
                     String filter = checkInput(scn, new String[]{"A","B","C","D","E"});
@@ -57,13 +65,16 @@ public class Main extends Methods {
                     loop = proceed();
                     break;
 
+                // Displaying information of a character
                 case "F":
                     System.out.println("Enter a charfacter name: ");
                     String name = checkInput(scn, chars.characterContainer);
 
                     for (int i = 0; i < chars.characterContainer.length; i++){
 
-                        name = name.replace(" ","");
+                        name = name.replace(" ",""); // To ensure no error happens due to space
+
+                        // Goes through the character container and if it matches, it will display the character information
                         if (chars.characterContainer[i].equalsIgnoreCase(name)){
                             System.out.println("Character:          Rarity:             Path:               Element:            Faction:");
                             printCharacterInfo(chars.characterContainer[i], chars.characterInformation[i]);
@@ -72,6 +83,7 @@ public class Main extends Methods {
                     loop = proceed();
                     break;
 
+                // Sorting Characters
                 case "G":
                     System.out.print("Sort Characters By: \nA: Alphabet\nB: Rarity\nC: Path\nD: Element\nE: Faction\n");
                     String sort = checkInput(scn, new String[]{"A","B","C","D","E"});
@@ -80,6 +92,7 @@ public class Main extends Methods {
                     break;
 
 
+                // Quitting program
                 case "Q":
                     System.out.println("Quitting program..");
                     loop = false;
