a6ad775f755581ffb601552541078bcc063e740d
Hoyoddiction <144769150+Hoyoddiction@users.noreply.github.com>
2024-06-02 22:40:18 +0700

    Update Character.java

--- HashTable (Character Object)/Character.java
+++ HashTable (Character Object)/Character.java
@@ -1,1 +1,59 @@
+public class Character {
+    String name, rarity, path, element, faction;
 
+    public  Character(String name, String rarity, String path, String element, String faction) {
+        this.name = name;
+        this.rarity = rarity;
+        this.path = path;
+        this.element = element;
+        this.faction = faction;
+
+        Character Acheron = new Character("Acheron", "5 Star", "Nihility", "Lightning", "Self Anihilator");
+        Character Argenti = new Character("Argenti", "5 Star", "Erudition", "Physical", "Knights of Beauty");
+        Character Arlan = new Character("Arlan", "4 Star", "Destruction", "Lightning", "Herta Space Station");
+        Character Asta = new Character("Asta", "4 Star", "Harmony", "Fire", "Herta Space Station");
+        Character Aventurine = new Character("Aventurine", "5 Star", "Preservation", "Imaginary", "IPC");
+        Character Bailu = new Character("Bailu", "5 Star", "Abundance", "Physical", "The Xianzhou Loufu");
+        Character Blackswan = new Character("Argenti", "5 Star", "Nihility", "Physical", "Garden of Recollection");
+        Character Blade = new Character("Blade", "5 Star", "Destruction", "Wind", "Stellaron Hunter");
+        Character Bronya = new Character("Bronya", "5 Star", "Harmony", "Wind", "Belobog");
+        Character Clara = new Character("Clara", "5 Star", "Destruction", "Physical", "Belobog");
+        Character Danheng = new Character("Danheng", "4 Star", "The Hunt", "Wind", "Astral Express");
+        Character Drratio = new Character("Dr.Ratio", "5 Star", "The Hunt", "Imaginary", "Intelligensia Guild");
+        Character Fuxuan = new Character("Fuxuan", "5 Star", "Preservation ", "Quantum", "The Xianzhou Loufu");
+        Character Gallagher = new Character("Gallagher", "4 Star", "Abundance", "Fire", "Penacony");
+        Character Gepard = new Character("Gepard", "5 Star", "Preservation", "Ice", "Belobog");
+        Character Guinafen = new Character("Guinafen", "4 Star", "Nihility", "Fire", "The Xianzhou Loufu");
+        Character Hanya = new Character("Hanya", "4 Star", "Harmony", "Physical", "The Xianzhou Loufu");
+        Character Herta = new Character("Herta", "4 Star", "Erudition", "Ice", "Herta Space Station");
+        Character Himeko = new Character("Himeko", "5 Star", "Erudition", "Fire", "Astral Express");
+        Character Hook = new Character("Hook", "4 Star", "Destruction", "Fire", "Belobog");
+        Character HuoHuo = new Character("Huo Huo", "5 Star", "Abundance", "Wind", "The Xianzhou Loufu");
+        Character ImbibitorLunae = new Character("Imbibitor Lunae ", "5 Star", "Destruction", "Imaginary", "Astral Express");
+        Character Jingyuan = new Character("Jingyuan ", "5 Star", "Erudition", "Lightning", "The Xianzhou Loufu");
+        Character Jingliu = new Character("Jingliu ", "5 Star", "Destruction", "Ice", "The Xianzhou Loufu");
+        Character Kafka = new Character("Kafka ", "5 Star", "Nihility", "Lightning", "Stellaron Hunter");
+        Character Luka = new Character("Luka ", "4 Star", "Nihility", "Physical", "Belobog");
+        Character Luocha = new Character("Luocha ", "5 Star", "Abundance", "Imaginary", "The Xianzhou Loufu");
+        Character Lynx = new Character("Lynx ", "4 Star", "Abundance", "Quantum", "Belobog");
+        Character March7th = new Character("March7th ", "4 Star", "Preservation", "Ice", "Astral Express");
+        Character Misha = new Character("Misha ", "4 Star", "Destruction", "Ice", "Penacony");
+        Character Natasha = new Character("Natasha ", "4 Star", "Abundance", "Physical", "Belobog");
+        Character Pela = new Character("Pela ", "4 Star", "Nihility", "Ice", "Belobog");
+        Character Qingque = new Character("Qingque ", "4 Star", "Erudition", "Quantum", "The Xianzhou Loufu");
+        Character Robin = new Character("Robin ", "5 Star", "Harmony", "Physical", "Penacony");
+        Character Ruanmei = new Character("Ruanmei ", "5 Star", "Harmony", "Ice", "Herta Space Station");
+        Character Sampo = new Character("Sampo ", "4 Star", "Nihility", "Wind", "Belobog");
+        Character Serval = new Character("Serval ", "4 Star", "Erudition", "Lightning", "Belobog");
+        Character Silverwolf = new Character("Silverwolf ", "5 Star", "Nihility", "Quantum", "Stellaron Hunter");
+        Character Sparkle = new Character("Sparkle ", "5 Star", "Harmony", "Quantum", "Mask Fools");
+        Character Sushang = new Character("Sushang ", "4 Star", "The Hunt", "Physical", "The Xianzhou Loufu");
+        Character Tingyun = new Character("Tingyun ", "4 Star", "Harmony", "Lightning", "The Xianzhou Loufu");
+        Character Topaz = new Character("Topaz", "5 Star", "The Hunt", "Fire", "IPC");
+        Character Trailblazer = new Character("Trailblazer", "5 Star", "Adaptive", "Adaptive", "Astral Express");
+        Character Welt = new Character("Welt", "5 Star", "Nihility", "Imaginary", "Astral Express");
+        Character Xueyi = new Character("Xueyi ", "4 Star", "Destruction", "Quantum", "The Xianzhou Loufu");
+        Character Yanging = new Character("Yanqing ", "5 Star", "The Hunt", "Ice", "The Xianzhou Loufu");
+        Character Yukong = new Character("Yukong ", "4 Star", "Harmony", "Imaginary", "The Xianzhou Loufu");
+    }
+}
