fdb64c00c5fb34af0ce0d4e0938ff249f8a4e4bd
Hoyoddiction <144769150+Hoyoddiction@users.noreply.github.com>
2024-06-04 18:20:42 +0700

    Update Character.java

--- HashTable (Character Object)/Character.java
+++ HashTable (Character Object)/Character.java
@@ -1,30 +1,40 @@
-public class Character{
+//  The Character class
+public class Character {
+
+    // Variables that hold the attributes of a character
     String name, rarity, path, element, faction;
 
+    // Constructor to initialize a Character object with specific attributes
     public Character(String name, String rarity, String path, String element, String faction) {
-        this.name = name;
-        this.rarity = rarity;
-        this.path = path;
-        this.element = element;
-        this.faction = faction;
+        this.name = name;       
+        this.rarity = rarity;   
+        this.path = path;       
+        this.element = element; 
+        this.faction = faction; 
     }
+
+    // Getter method for the faction attribute
     public String getFaction() {
-        return faction;
+        return faction; 
     }
+
+    // Getter method for the element attribute
     public String getElement() {
-        return this.element;
+        return this.element; 
     }
 
+    // Getter method for the path attribute
     public String getPath() {
-        return path;
+        return path; 
     }
 
+    // Getter method for the name attribute
     public String getName() {
-        return name;
-
+        return name; 
     }
+
+    // Getter method for the rarity attribute
     public String getRarity() {
-        return rarity;
+        return rarity; 
     }
 }
-
